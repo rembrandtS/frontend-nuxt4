b908717fd801feedf441dde4e1929f069b5d0115
rembrandt <remblandt@gmail.com>
2025-10-18 16:55:40 +0900

    키움증권 주문 보완 - 2

---
+++ app/layouts/news.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+      <AppHeader />
+      <AppNav />
+      <slot />
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app/pages/news/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import CardComponent from '~/pages/news/components/Card.vue';
+import {useStore} from '~/stores/api';
+
+definePageMeta({
+  layout: 'news',
+})
+
+const store = useStore();
+const {pending} = await useAsyncData("getNews", store.getNews);
+</script>
+
+<template>
+<div class="page">
+  <CardComponent v-for="article in store.articles" :key="article.url" :data="article" />
+</div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: center;
+
+  width: calc(100% - 96px);
+  padding: 48px;
+  gap: 24px;
+}
+</style>
